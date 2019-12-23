10fb1db272dccb417351f73cd8e87f1a4a9d2ab8
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-23 14:31:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,14 +3,14 @@
 LINE = "-----------"
 
 #Prints out one of the rows filled with the appropriate things
-def row(row_array)
+def rowPrint(row_array)
   puts " #{row_array[0]} | #{row_array[1]} | #{row_array[2]} "
 end
 
 def display_board(board)
-  puts row(board[0..2])
+  puts rowPrint(board[0..2])
   puts LINE
-  puts row(board[3..6])
+  puts rowPrint(board[3..6])
   puts LINE
-  puts row(board[7..9])
+  puts rowPrint(board[7..9])
 end
