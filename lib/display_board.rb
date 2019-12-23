540e0afe6806ef9cd3e2546f7ca43792f39a9275
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-23 14:27:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+LINE = "-----------"
+
+#Prints out one of the rows filled with the appropriate things
+def row(row_array)
+  puts " #{row_array[0]} | #{row_array[1]} | #{row_array[2]} "
+end
+
+def display_board(board)
+  row(board[])
